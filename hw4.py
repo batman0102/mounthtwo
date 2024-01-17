fc3358f0952318d3434a30c6282f87b7648dcc0a
batman0102 <palachka01@gmail.com>
2024-01-17 15:43:13 +0300

    hw4

---
+++ hw4.py
@@ -0,0 +1,32 @@
+
+class BankAccount:
+    pass
+
+class SavingAccount(BankAccount):
+    pass
+
+class CheckingAccount(BankAccount):
+    pass
+
+class InterestBearingItem:
+    pass
+
+class Asset:
+    pass
+
+class RealEstate:
+    pass
+
+class InsurableItem:
+    pass
+
+class Stock:
+    pass
+
+class Security:
+    pass
+
+class Band:
+    pass
+
+
